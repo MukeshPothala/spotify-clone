f5b3682991bfc083fc665f24ea6e7e7c1df3b2a2
MukeshPothala <mukeshpothala189@gmail.com>
2023-12-19 23:39:46 +0530

    eslint: {         ignoreDuringBuilds: true,     },

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
     images: {
         domains: ['preview.redd.it',"www.iphonefaq.org","uhrdznuqfgqaffuitvnu.supabase.co"],
     }
